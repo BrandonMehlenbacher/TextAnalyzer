fa813d3e629b63e0514237be54a601188ba3a421
Brandon <bmehlenbacher4@gmail.com>
2021-02-10 17:07:50 -0600

    initial commit

---
+++ TextAnalyzer.py
@@ -0,0 +1,24 @@
+
+
+class TextAnalyzer:
+    """
+    The idea behind this class will be to analyze read in a text file and perform some sort of analysis
+    on the file to find something
+    """
+    def __init__(self,file):
+        self.file = file
+    def findNames(self):
+        pass
+    def printNames(self):
+        pass
+class PysideAnalyzer(TextAnalyzer):
+    def __inti__(self):
+        super().__init__()
+    def findCheckBox(self):
+        pass
+    def findNumber(self):
+        pass
+    def selectFind(self):
+        pass
+    def findAll(self):
+        pass
