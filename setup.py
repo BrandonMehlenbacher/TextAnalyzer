b5186630725ed48b2aed48e9737d6739a38cbf05
Brandon Mehlenbacher <48697260+BrandonMehlenbacher@users.noreply.github.com>
2021-02-10 17:15:29 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+  name    = "TextAnalyzer",
+  version = "0.0.1",
+  install_requires =[
+    "pandas",],
+ description= "Analyzer for python files including Pyside2",
+ authors="Brandon Mehlenbacher",
+ license="MIT",
+ classifiers=[
+   "Intended Audience :: Science/Research",
+   "License :: OSI Approved :: MIT License",
+   "Natural Language :: English",
+   "Programming Language :: Python :: 3",
+   "Programming Language :: Python :: 3.7",
+   "Programming Language :: Python :: 3.8",
+   "Topic :: Scientific/Engineering",]
+)
